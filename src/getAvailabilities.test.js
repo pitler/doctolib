3a037801cb6f081793f72a51e040cefba8e0d7e6
pitler <pitler@gmail.com>
2019-08-15 14:53:06 -0500

    Fix error 1

--- src/getAvailabilities.test.js
+++ src/getAvailabilities.test.js
@@ -47,7 +47,7 @@ describe("getAvailabilities", () => {
         "9:30",
         "10:00",
         "11:30",
-        „14:00"
+        "14:00"
       ]);
 
       expect(String(availabilities[6].date)).toBe(
